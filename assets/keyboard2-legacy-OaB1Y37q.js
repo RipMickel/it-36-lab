b2020eb82f6cdf46d6ef010cbff6e0e91bc300ca
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-28 12:15:03 +0800

    Updates

--- assets/keyboard2-legacy-OaB1Y37q.js
+++ assets/keyboard2-legacy-OaB1Y37q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnoQXa2F.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D_au4WZC.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
